8aa1eb2462c06f817eb7981bcb8afdf615fc0029
jack mead <jack-mead@hotmail.co.uk>
2018-06-02 23:37:46 +0100

    Working versions of registering, logging in and logging out but with frontend currently passing through dummy info

--- WorldBuilder/WorldBuilder/Controllers/HomeController.cs
+++ WorldBuilder/WorldBuilder/Controllers/HomeController.cs
@@ -1,5 +1,12 @@
 ﻿using DataAccess.Access;
+using DataAccess.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
+using WorldBuilder.Helpers;
 
 namespace WorldBuilder.Controllers
 {
@@ -15,6 +22,38 @@ namespace WorldBuilder.Controllers
             ViewBag.Message = "Welcome to world builder";
 
             return View();
+        }
+
+        public ActionResult DummyLogin()
+        {
+            var userManager = HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            var authManager = HttpContext.GetOwinContext().Authentication;
+
+            User user = userManager.Find("a.User", "password");
+            var ident = userManager.CreateIdentity(user,
+                DefaultAuthenticationTypes.ApplicationCookie);
+            authManager.SignIn(
+                new AuthenticationProperties { IsPersistent = false }, ident);
+            return Redirect(Url.Action("Index", "Home"));
+        }
+
+        public async Task<ActionResult> DummyRegister()
+        {
+            var userManager = HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            var result = await userManager.CreateAsync(new User { UserName = "a.User" }, "password");
+            if (result.Succeeded)
+            {
+                return Redirect(Url.Action("DummyLogin", "Home"));
+            }
+            return Redirect(Url.Action("About", "Home"));
+        }
+
+        public ActionResult DummyLogout()
+        {
+
+            var authManager = HttpContext.GetOwinContext().Authentication;
+            authManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return Redirect(Url.Action("About", "Home"));
         }
     }
 }
