12bc983ccbd1df28138b90b86c1b2f45db2877a3
jack mead <jack-mead@hotmail.co.uk>
2018-06-05 18:50:00 +0100

    Added extra pages and found potential banner image

---
+++ WorldBuilder/WorldBuilder/Controllers/WorldController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorldBuilder.Controllers
+{
+    public class WorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
